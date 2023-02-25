a23a137501bfa055e30a13f36a30d2c12b720051
fidelsiahaan <fidelsiahaan@tamu.edu>
2023-02-25 15:18:39 -0600

    Feb25(WhileWorking)

--- app/src/main/java/com/example/user_profile/ProfilePage.java
+++ app/src/main/java/com/example/user_profile/ProfilePage.java
@@ -25,10 +25,11 @@ public class ProfilePage extends AppCompatActivity{
 
 
     private Button userSummaryButton;
-    private TextView name;
+    private TextView name, weight, age, email, gender, heightft, heightin;
 
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference database, personal, random, nameVal;
+    DatabaseReference database, personal, id, nameRef, weightRef, ageRef, emailRef, genderRef,
+    heightFtRef, heightInRef;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +38,35 @@ public class ProfilePage extends AppCompatActivity{
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         database = firebaseDatabase.getReference("Users");
-        random = database.child("WpsNdVWH43gNSscAI69Un1L3vQJ3");
-        personal = random.child("Personal Information");
-        nameVal = personal.child("name");
+        id = database.child("b1KqcJul0WZOdcXRdaJ2wuKPbLL2"); //Personal user id
+        personal = id.child("Personal Information");
+
+        nameRef = personal.child("name");
+        ageRef = personal.child("age");
+        weightRef = personal.child("weight");
+        emailRef = personal.child("email");
+        genderRef = personal.child("gender");
+        heightFtRef = personal.child("feet");
+        heightInRef = personal.child("inches");
 
         name = findViewById(R.id.nameValue);
+        weight = findViewById(R.id.weightValue);
+        age = findViewById(R.id.ageValue);
+        email = findViewById(R.id.emailValue);
+        gender = findViewById(R.id.genderValue);
+        heightft = findViewById(R.id.heightFeetValue);
+        heightin = findViewById(R.id.heightInValue);
 
-        getdata();
+
+
+        getdata(nameRef, name);
+        getdataNum(weightRef, weight);
+        getdataNum(ageRef, age);
+        getdata(emailRef, email);
+        getdata(genderRef, gender);
+        getdataNum(heightFtRef, gender);
+        getdataNum(heightInRef, gender);
+
 
         userSummaryButton = (Button) findViewById(R.id.userSummaryButton);
     userSummaryButton.setOnClickListener((new View.OnClickListener(){
@@ -54,13 +77,26 @@ public class ProfilePage extends AppCompatActivity{
     }));
     }
 
-
-    private void getdata() {
-        nameVal.addValueEventListener(new ValueEventListener() {
+    private void getdata(DatabaseReference ref, TextView text) {
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String value = snapshot.getValue(String.class);
-                name.setText(value);
+                text.setText(value);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfilePage.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getdataNum(DatabaseReference ref, TextView text) {
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int value = snapshot.getValue(int.class);
+                text.setText(String.valueOf(value));
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
